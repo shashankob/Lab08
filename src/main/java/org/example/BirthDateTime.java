d86ec727fbca6d65b256ac24168ec1c57726b950
shash <obulassk@mail.uc.edu>
2023-07-03 10:30:20 -0400

    Third Commit

--- src/main/java/org/example/BirthDateTime.java
+++ src/main/java/org/example/BirthDateTime.java
@@ -8,26 +8,26 @@ public class BirthDateTime {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int year = SafeInput.getRangedInt(in, "Enter the year of your birth:", 1950, 2015);
-        int month = SafeInput.getRangedInt(in, "Enter the month of your birth:", 1, 12);
-        int day = 0;
+        int yearPro = SafeInput.getRangedInt(in, "Enter the year of your birth:", 1950, 2015);
+        int monthPro = SafeInput.getRangedInt(in, "Enter the month of your birth:", 1, 12);
+        int dayPro = 0;
 
-        switch (month) {
+        switch (monthPro) {
             case 2: // February
-                day = SafeInput.getRangedInt(in, "Enter the day of your birth (1-29):", 1, 29);
+                dayPro = SafeInput.getRangedInt(in, "Enter the day of your birth (1-29):", 1, 29);
             case 4: // April
             case 6: // June
             case 9: // September
             case 11: // November
-                day = SafeInput.getRangedInt(in, "Enter the day of your birth (1-30):", 1, 30);
+                dayPro = SafeInput.getRangedInt(in, "Enter the day of your birth (1-30):", 1, 30);
                 break;
             default:
-                day = SafeInput.getRangedInt(in, "Enter the day of your birth (1-31):", 1, 31);
+                dayPro = SafeInput.getRangedInt(in, "Enter the day of your birth (1-31):", 1, 31);
         }
-        int hours = SafeInput.getRangedInt(in, "Enter the hours of your birth:", 1, 24);
-        int minutes = SafeInput.getRangedInt(in, "Enter the minutes of your birth:", 1, 59);
+        int hourPro = SafeInput.getRangedInt(in, "Enter the hours of your birth:", 1, 24);
+        int minutesPro = SafeInput.getRangedInt(in, "Enter the minutes of your birth:", 1, 59);
 
-        System.out.println("Your date of birth is: " + month + "/" + day + "/" + year);
-        System.out.println("Your time of birth is: " + hours + ":" + minutes);
+        System.out.println("Your date of birth is: " + monthPro + "/" + dayPro + "/" + yearPro);
+        System.out.println("Your time of birth is: " + hourPro + ":" + minutesPro);
     }
 }
